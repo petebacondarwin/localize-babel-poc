d8f32d2845df97d955425bc26942652d37099e0e
Pete Bacon Darwin <pete@bacondarwin.com>
2019-08-09 16:26:25 +0100

    tidy up test file

--- dist/src.js
+++ dist/src.js
@@ -1,21 +1,20 @@
 function foo(a) {
-  var b = 10; // tagged string, with binary expression
+  var b = 10;
+  "try\n" + (40 + b) + "\n  me"; // tagged string, with binary expression
 
-  "try\n" + (40 + 2) + "\n  me"; // nested tags
+  "a" + 1 + "b" + ("x" + 5 + "y" + 6 + "z") + "c"; // nested tags
 
-  "a" + 1 + "b" + ("x" + 5 + "y" + 6 + "z") + "c"; // es5
+  'try' + 40 + 'me'; // es5
 
-  'try' + 40 + 'me'; // embedded
+  'a' + 1 + 'b' + ('x' + 5 + 'y' + 6 + 'z') + 'c'; // embedded
 
-  'a' + 1 + 'b' + ('x' + 5 + 'y' + 6 + 'z') + 'c'; // helper
+  'try' + (40 + 2) + 'me'; // helper
 
-  'try' + (40 + 2) + 'me';
-  'try' + (40 + 2) + 'me'; // inline helper
+  'try' + (40 + 2) + 'me'; // aliased helper
 
-  'try' + 40 + 'me';
+  'try' + 40 + 'me'; // inline helper
 }
 
 function bar(localize) {
-  // Not the global
-  localize(['goodbye']);
+  localize(['goodbye']); // Not the global `localize`
 }
--- test/src.js
+++ test/src.js
@@ -1,28 +1,15 @@
 function foo(a) {
   var b = 10;
-
-  // tagged string, with binary expression
-  localize`try\n${40 + 2}
-  me`;
-
-  // nested tags
-  localize`a${1}b${localize`x${5}y${6}z`}c`;
-
-  // es5
-  localize(['try', 'me'], 40, 2);
-
-  // embedded
-  localize(['a', 'b', 'c'], 1, localize(['x', 'y', 'z'], 5, 6));
-
-  // helper
-  localize(__makeTemplateObject(['try', 'me'], ['try', 'me']), 40 + 2);
-  localize(m(['try', 'me'], ['try', 'me']), 40 + 2);
-
-  // inline helper
-  localize((this&&this.__makeTemplateObject||function(e,t){return Object.defineProperty?Object.defineProperty(e,"raw",{value:t}):e.raw=t,e})(['try', 'me'], ['try', 'me']), 40, 2);
+  localize`try\n${40 + b}
+  me`;  // tagged string, with binary expression
+  localize`a${1}b${localize`x${5}y${6}z`}c`;  // nested tags
+  localize(['try', 'me'], 40, 2);  // es5
+  localize(['a', 'b', 'c'], 1, localize(['x', 'y', 'z'], 5, 6));  // embedded
+  localize(__makeTemplateObject(['try', 'me'], ['try', 'me']), 40 + 2);  // helper
+  localize(m(['try', 'me'], ['try', 'me']), 40 + 2);   // aliased helper
+  localize((this&&this.__makeTemplateObject||function(e,t){return Object.defineProperty?Object.defineProperty(e,"raw",{value:t}):e.raw=t,e})(['try', 'me'], ['try', 'me']), 40, 2);  // inline helper
 }
 
 function bar(localize) {
-  // Not the global
-  localize(['goodbye']);
+  localize(['goodbye']);  // Not the global `localize`
 }
